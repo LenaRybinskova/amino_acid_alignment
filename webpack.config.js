705e8f230f5183a7c76c1c7f868f493ab6d0bb32
«Elena <LenaRybinskova@gmail.com>
2025-06-10 15:41:54 +0300

    config deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development', // для продакшена меняй на 'production'
+    mode: 'development',
     entry: './src/index.tsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true, // очищать dist перед сборкой
+        clean: true,
+        publicPath: '/amino_acid_sequence/',
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
-    devtool: 'inline-source-map', // для дебага
+    devtool: 'inline-source-map',
     devServer: {
         static: './dist',
         hot: true,
