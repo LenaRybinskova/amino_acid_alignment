66bae40766b08af961a92771dddb97fefc05d838
«Elena <LenaRybinskova@gmail.com>
2025-06-11 12:37:34 +0300

    rename

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        publicPath: '/amino_acid_sequence/',
+        publicPath: '/amino_acid_alignment/',
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
